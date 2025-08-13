4cec0b315e429b5b9cb9753f0a3d87a69d472b6c
InsaneFirebat <InsaneFirebat@gmail.com>
2025-08-13 15:36:21 -0400

    Preparing to add a menu

--- main.asm
+++ main.asm
@@ -82,29 +82,8 @@ lorom
 !HUD_DECIMAL = $004D
 
 
-macro a8() ; A = 8-bit
-    SEP #$20
-endmacro
-
-macro a16() ; A = 16-bit
-    REP #$20
-endmacro
-
-macro i8() ; X/Y = 8-bit
-    SEP #$10
-endmacro
-
-macro i16() ; X/Y = 16-bit
-    REP #$10
-endmacro
-
-macro ai8() ; A + X/Y = 8-bit
-    SEP #$30
-endmacro
-
-macro ai16() ; A + X/Y = 16-bit
-    REP #$30
-endmacro
+incsrc defines.asm
+incsrc macros.asm
 
 
 ; Patch out copy protection
@@ -525,6 +504,8 @@ ih_game_loop_code:
     ; inc transition timer
     LDA !ram_transition_counter : INC : STA !ram_transition_counter
 
+    JSL MagicPants
+
     ; overwritten code + return
     JML $808111
 }
@@ -704,3 +685,40 @@ org $80FF10
 
 org $80FFA0
     db "Blame mm2"
+
+
+!SAMUS_MOVEMENT_TYPE = $0A1F
+!SAMUS_ANIMATION_FRAME = $0A96
+!ram_magic_pants_state = !WRAM_START+$12
+!ram_magic_pants_pal1 = !WRAM_START+$14
+!ram_magic_pants_pal2 = !WRAM_START+$16
+!ram_magic_pants_pal3 = !WRAM_START+$18
+
+
+org $9BCC00
+incsrc menu.asm
+
+;MagicPants:
+;{
+;    LDA $0A96 : CMP #$0009 : BEQ .check
+;    LDA !ram_magic_pants_state : BEQ +
+;    LDA !ram_magic_pants_pal1 : STA $7EC188
+;    LDA !ram_magic_pants_pal2 : STA $7EC18A
+;    LDA !ram_magic_pants_pal3 : STA $7EC19E
+;    LDA #$0000 : STA !ram_magic_pants_state
+;+   RTL
+;
+;  .check
+;    LDA $0A1C : CMP #$0009 : BEQ .flash
+;    CMP #$000A : BEQ .flash
+;    RTL
+;
+;  .flash
+;    LDA !ram_magic_pants_state : BNE +
+;    LDA $7EC188 : STA !ram_magic_pants_pal1
+;    LDA $7EC18A : STA !ram_magic_pants_pal2
+;    LDA $7EC19E : STA !ram_magic_pants_pal3
+;+   LDA #$FFFF : STA $7EC19E : STA !ram_magic_pants_state
+;    RTL
+;}
+

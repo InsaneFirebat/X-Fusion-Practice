6e8fbab4b15673be2a6f5b64a7f6f7d044758bd1
Mr. Mendelli <47702158+MrMendelli@users.noreply.github.com>
2025-08-21 06:45:29 -0600

    Minor Update * Updated all symbol, patch, and ROM names to be consistent with one anotehr * Consolidated symbol output to \build\ directory

--- build_ips.bat
+++ build_ips.bat
@@ -4,21 +4,21 @@ echo.
 echo Generating practice hack symbols...
 echo.
 python ".\create_dummies.py" ".\build\00.sfc" ".\build\ff.sfc"
-".\asar\asar.exe" --no-title-check --symbols=wla --symbols-path=".\X-Fusion_Symbols.sym" -DSAVESTATES=0 ".\main.asm" ".\build\00.sfc"
+".\asar\asar.exe" --no-title-check --symbols=wla --symbols-path=".\build\X-Fusion Practice Hack.sym" -DSAVESTATES=0 ".\main.asm" ".\build\00.sfc"
 ".\asar\asar.exe" --no-title-check -DSAVESTATES=0 ".\main.asm" ".\build\ff.sfc"
 echo.
 echo Building practice hack patch...
-python ".\create_ips.py" ".\build\00.sfc" ".\build\ff.sfc" ".\build\X-Fusion_Practice.ips"
+python ".\create_ips.py" ".\build\00.sfc" ".\build\ff.sfc" ".\build\X-Fusion Practice Hack.ips"
 del ".\build\00.sfc" ".\build\ff.sfc" /s /q > nul
 
 echo.
 echo Generating savestate practice hack symbols...
 echo.
 python ".\create_dummies.py" ".\build\00.sfc" ".\build\ff.sfc"
-".\asar\asar.exe" --no-title-check --symbols=wla --symbols-path=".\X-Fusion_Savestate_Symbols.sym" ".\main.asm" ".\build\00.sfc"
+".\asar\asar.exe" --no-title-check --symbols=wla --symbols-path=".\build\X-Fusion Practice Hack (Savestates).sym" ".\main.asm" ".\build\00.sfc"
 ".\asar\asar.exe" --no-title-check ".\main.asm" ".\build\ff.sfc"
 echo.
 echo Building savestate practice hack patch...
-python ".\create_ips.py" ".\build\00.sfc" ".\build\ff.sfc" ".\build\X-Fusion_Practice_Savestates.ips"
+python ".\create_ips.py" ".\build\00.sfc" ".\build\ff.sfc" ".\build\X-Fusion Practice Hack (Savestates).ips"
 del ".\build\00.sfc" ".\build\ff.sfc" /s /q > nul
 pause

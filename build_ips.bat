f9a5a9036c8800759c561cbb13f9901f84f0803e
InsaneFirebat <InsaneFirebat@gmail.com>
2025-08-21 09:35:16 -0400

    Point batches to src directory

--- build_ips.bat
+++ build_ips.bat
@@ -4,8 +4,8 @@ echo.
 echo Generating practice hack symbols...
 echo.
 python ".\create_dummies.py" ".\build\00.sfc" ".\build\ff.sfc"
-".\asar\asar.exe" --no-title-check --symbols=wla --symbols-path=".\build\X-Fusion Practice Hack.sym" -DSAVESTATES=0 ".\main.asm" ".\build\00.sfc"
-".\asar\asar.exe" --no-title-check -DSAVESTATES=0 ".\main.asm" ".\build\ff.sfc"
+".\asar\asar.exe" --no-title-check --symbols=wla --symbols-path=".\build\X-Fusion Practice Hack.sym" -DSAVESTATES=0 ".\src\main.asm" ".\build\00.sfc"
+".\asar\asar.exe" --no-title-check -DSAVESTATES=0 ".\src\main.asm" ".\build\ff.sfc"
 echo.
 echo Building practice hack patch...
 python ".\create_ips.py" ".\build\00.sfc" ".\build\ff.sfc" ".\build\X-Fusion Practice Hack.ips"
@@ -15,8 +15,8 @@ echo.
 echo Generating savestate practice hack symbols...
 echo.
 python ".\create_dummies.py" ".\build\00.sfc" ".\build\ff.sfc"
-".\asar\asar.exe" --no-title-check --symbols=wla --symbols-path=".\build\X-Fusion Practice Hack (Savestates).sym" ".\main.asm" ".\build\00.sfc"
-".\asar\asar.exe" --no-title-check ".\main.asm" ".\build\ff.sfc"
+".\asar\asar.exe" --no-title-check --symbols=wla --symbols-path=".\build\X-Fusion Practice Hack (Savestates).sym" ".\src\main.asm" ".\build\00.sfc"
+".\asar\asar.exe" --no-title-check ".\src\main.asm" ".\build\ff.sfc"
 echo.
 echo Building savestate practice hack patch...
 python ".\create_ips.py" ".\build\00.sfc" ".\build\ff.sfc" ".\build\X-Fusion Practice Hack (Savestates).ips"

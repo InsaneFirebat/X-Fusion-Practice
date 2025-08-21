48ca51d7e84abba75419661b961041347f47e7f8
Mr. Mendelli <47702158+MrMendelli@users.noreply.github.com>
2025-08-21 04:39:32 -0600

    Improved Automation + Patching script now automatically generates symbols if they are missing + Rewrote build_ips.bat + Updated readme

--- build_ips.bat
+++ build_ips.bat
@@ -1,24 +1,24 @@
 @echo off
 
-echo Building X-Fusion Practice with savestates
-python create_dummies.py 00.sfc ff.sfc
+echo.
+echo Generating practice hack symbols...
+echo.
+python ".\create_dummies.py" ".\build\00.sfc" ".\build\ff.sfc"
+".\asar\asar.exe" --no-title-check --symbols=wla --symbols-path=".\X-Fusion_Symbols.sym" -DSAVESTATES=0 ".\main.asm" ".\build\00.sfc"
+".\asar\asar.exe" --no-title-check -DSAVESTATES=0 ".\main.asm" ".\build\ff.sfc"
+echo.
+echo Building practice hack patch...
+python ".\create_ips.py" ".\build\00.sfc" ".\build\ff.sfc" ".\build\X-Fusion_Practice.ips"
+del ".\build\00.sfc" ".\build\ff.sfc" /s /q > nul
 
-copy *.sfc build
-asar\asar.exe --no-title-check --symbols=wla --symbols-path=X-Fusion_Savestate_Symbols.sym src/main.asm build\00.sfc
-asar\asar.exe --no-title-check src/main.asm build\ff.sfc
-python create_ips.py build\00.sfc build\ff.sfc build\X-Fusion_Practice_Savestates.ips
-
-del 00.sfc ff.sfc build\00.sfc build\ff.sfc
-
-
-echo Building X-Fusion Practice
-python create_dummies.py 00.sfc ff.sfc
-
-copy *.sfc build
-asar\asar.exe --no-title-check --symbols=wla --symbols-path=X-Fusion_Symbols.sym -DSAVESTATES=0 src/main.asm build\00.sfc
-asar\asar.exe --no-title-check -DSAVESTATES=0 src/main.asm build\ff.sfc
-python create_ips.py build\00.sfc build\ff.sfc build\X-Fusion_Practice.ips
-
-del 00.sfc ff.sfc build\00.sfc build\ff.sfc
-
-PAUSE
+echo.
+echo Generating savestate practice hack symbols...
+echo.
+python ".\create_dummies.py" ".\build\00.sfc" ".\build\ff.sfc"
+".\asar\asar.exe" --no-title-check --symbols=wla --symbols-path=".\X-Fusion_Savestate_Symbols.sym" ".\main.asm" ".\build\00.sfc"
+".\asar\asar.exe" --no-title-check ".\main.asm" ".\build\ff.sfc"
+echo.
+echo Building savestate practice hack patch...
+python ".\create_ips.py" ".\build\00.sfc" ".\build\ff.sfc" ".\build\X-Fusion_Practice_Savestates.ips"
+del ".\build\00.sfc" ".\build\ff.sfc" /s /q > nul
+pause
